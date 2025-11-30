13be7dd5a75576bf64549d8141deca05082f394d
Aditya41150 <singhadi437@gmail.com>
2025-11-30 13:55:55 +0530

    Fix Vite base for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/Eazy-Payouts/', 
+  base: '/', // important for Netlify root deploy
 })
